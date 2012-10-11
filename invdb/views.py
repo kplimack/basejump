05d91b764860325031b27ffeb7f82369ef7947c4
Kyle Jake Plimack <kyle.plimack@gmail.com>
2012-10-11 10:09:48 -0400

    build the menus a little more cleanly

--- invdb/views.py
+++ invdb/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib.auth.models import *
 from django.contrib.auth import login, authenticate, logout
+from invdb.models import *
+from basejump.plural import plural
 
 def index(request):
     if not request.user.is_authenticated():
@@ -11,5 +13,25 @@ def index(request):
                                   context_instance=RequestContext(request))
     else:
         user = request.user
-        return render_to_response('index.html',
+        content_bag = {
+            'nav_left_menu': menutize(getAssetTypes()),
+            }
+        print "\n\nCONTENT BAG\n%s\n\n" % content_bag
+        return render_to_response('index.html', content_bag,
                                   context_instance=RequestContext(request))
+
+
+def menutize(boo):
+    # boo: bunch of objects
+    menu = []
+    for o in boo:
+        menuitem = {
+            'name': plural(o.name),
+            'url': '#',
+            }
+        menu.append(menuitem)
+    return menu
+
+def getAssetTypes():
+    results = AssetType.objects.all().order_by('name')
+    return results
