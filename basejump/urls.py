702dd3fe1d8402b69dc3ae7f9bf0df6912892cd4
Kyle Jake Plimack <kyle.plimack@gmail.com>
2012-10-11 10:29:29 -0400

    add invdb models to admin interface '

--- basejump/urls.py
+++ basejump/urls.py
@@ -2,15 +2,12 @@ from django.conf.urls import patterns, include, url, static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from invdb import views
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 #handler404 = views.handler404
 
 urlpatterns = patterns('',
-#                       url(r'^logout', 'authenticator.views.logout_user', name='logout_user'),
- #                      url(r'^login', 'authenticator.views.login_user', name='login_user'),
                        url('^', include('authenticator.urls')),
                        url('^invdb/', include('invdb.urls')),
  #   url('^importer/', include('importer.urls')),
---
+++ invdb/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from invdb.models import *
+
+admin.site.register(Asset)
+admin.site.register(Area)
+admin.site.register(Rack),
+admin.site.register(AssetType)
+admin.site.register(Role)
+admin.site.register(LogicalStatusCode)
+admin.site.register(PhysicalStatusCode)
+
