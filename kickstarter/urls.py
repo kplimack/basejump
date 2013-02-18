3bbbc15d445cbda3c3610378fbffec4cb8789a07
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-02-17 20:32:35 -0500

    launch page for kickstarter

---
+++ kickstarter/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from kickstarter import views
+from django.contrib import admin
+admin.autodiscover()
+
+#handler404 = views.handler404
+
+urlpatterns = patterns('kickstarter.views',
+                       url(r'^$', 'index', name='index'),
+                   )
+
+urlpatterns += staticfiles_urlpatterns()
+
