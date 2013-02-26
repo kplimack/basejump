e8f8cba1d063f2ce297db4293d4db797a1e1f97b
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-02-26 01:58:43 -0500

    building the beginning of an IPMI library

---
+++ basejump/ipmi.py
@@ -0,0 +1,22 @@
+from subprocess import Popen, PIPE
+import sys
+
+def getPower(host, user="root", password="calvin"):
+    cmd = "/usr/local/bin/ipmitool -H " + host + " -U " + user + " -P " + password + " power status"
+    p = subprocess.Popen(cmd, stdout=PIPE, shell=True)
+    print "getPower(%s) = %s)" % (host, p.stdout.read())
+
+def powerCycle(host, user="root", password="calvin"):
+    cmd = "/usr/local/bin/ipmitool -H " + host + " -U " + user + " -P " + password + "power cycle"
+    p = subprocess.Popen(cmd, stdout=PIPE, shell=True)
+    print "powerCycle(%s) = %s)" % (host, p.stdout.read())
+
+def powerOn(host, user="root", password="calvin"):
+    cmd = "/usr/local/bin/ipmitool -H " + host + " -U " + user + " -P " + password + "power on"
+    p = subprocess.Popen(cmd, stdout=PIPE, shell=True)
+    print "powerCycle(%s) = %s)" % (host, p.stdout.read())
+
+def powerOff(host, user="root", password="calvin"):
+    cmd = "/usr/local/bin/ipmitool -H " + host + " -U " + user + " -P " + password + "power off"
+    p = subprocess.Popen(cmd, stdout=PIPE, shell=True)
+    print "powerCycle(%s) = %s)" % (host, p.stdout.read())
