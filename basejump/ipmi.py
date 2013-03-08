0df239a677de62ee75ae3f061b4e8cce84c3b893
Kyle Jake Plimack <kyle.plimack@gmail.com>
2013-03-08 13:01:25 -0500

    specify auth for ipmi

--- basejump/ipmi.py
+++ basejump/ipmi.py
@@ -3,22 +3,22 @@ import subprocess
 import sys
 
 def getPower(host, user="root", password="calvin"):
-    cmd = "/usr/bin/ipmitool -H " + host + " -U " + user + " -P " + password + " power status"
+    cmd = "/usr/bin/ipmitool -C3 -H " + host + " -U " + user + " -P " + password + " power status"
     p = subprocess.Popen(cmd, stdout=PIPE, shell=True)
     print "getPower(%s) = %s)" % (host, p.stdout.read())
 
 def powerCycle(host, user="root", password="calvin"):
-    cmd = "/usr/bin/ipmitool -H " + host + " -U " + user + " -P " + password + " power cycle"
+    cmd = "/usr/bin/ipmitool -C3 -H " + host + " -U " + user + " -P " + password + " power cycle"
     print "CMD: %s" % cmd
     p = subprocess.Popen(cmd, stdout=PIPE, shell=True)
     print "powerCycle(%s) = %s)" % (host, p.stdout.read())
 
 def powerOn(host, user="root", password="calvin"):
-    cmd = "/usr/bin/ipmitool -H " + host + " -U " + user + " -P " + password + " power on"
+    cmd = "/usr/bin/ipmitool -C3 -H " + host + " -U " + user + " -P " + password + " power on"
     p = subprocess.Popen(cmd, stdout=PIPE, shell=True)
     print "powerCycle(%s) = %s)" % (host, p.stdout.read())
 
 def powerOff(host, user="root", password="calvin"):
-    cmd = "/usr/bin/ipmitool -H " + host + " -U " + user + " -P " + password + " power off"
+    cmd = "/usr/bin/ipmitool -C3 -H " + host + " -U " + user + " -P " + password + " power off"
     p = subprocess.Popen(cmd, stdout=PIPE, shell=True)
     print "powerCycle(%s) = %s)" % (host, p.stdout.read())
